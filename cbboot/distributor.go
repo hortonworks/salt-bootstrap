98014abea055299032892c5129dc2fb9c29028a2
Attila Kanto <attila.kanto@gmail.com>
2016-05-01 15:41:13 +0200

    Better distrubutor implementation using Payload interface

--- cbboot/distributor.go
+++ cbboot/distributor.go
@@ -12,6 +12,61 @@ import (
     "strconv"
 )
 
+type Payload interface {
+    AsByteArray() []byte
+}
+
+
+func DistributePayload(clients []string, payloads []Payload, endpoint string, user string, pass string) <-chan model.Response {
+    var wg sync.WaitGroup
+    wg.Add(len(clients))
+    c := make(chan model.Response, len(clients))
+
+    for idx, client := range clients {
+        go func(client string) {
+            defer wg.Done()
+            log.Printf("[distribute] Send request to client: %s", client)
+            log.Printf("[distribute] Request payload: %s", string(payloads[idx].AsByteArray()))
+
+            var clientAddr string
+            if (strings.Contains(client, ":")) {
+                clientAddr = client
+            } else {
+                clientAddr = client + ":" + strconv.Itoa(DetermineBootstrapPort())
+            }
+
+            req, err := http.NewRequest("POST", "http://" + clientAddr + endpoint, bytes.NewBuffer(payloads[idx].AsByteArray()))
+            req.Header.Set("Content-Type", "application/json")
+            req.SetBasicAuth(user, pass)
+
+            httpClient := &http.Client{}
+            resp, err := httpClient.Do(req)
+            if err != nil {
+                log.Printf("[distribute] Failed to send request to: %s, error: %s", client, err.Error())
+                c <- model.Response{StatusCode:http.StatusInternalServerError, ErrorText:err.Error(), Address:client}
+                return
+            }
+
+            body, _ := ioutil.ReadAll(resp.Body)
+            decoder := json.NewDecoder(strings.NewReader(string(body)))
+            var response model.Response
+            decoder.Decode(&response)
+            response.Address = client
+
+            if response.StatusCode == 0 {
+                response.StatusCode = resp.StatusCode
+            }
+            log.Printf("[distribute] Request to: %s result: %s", client, response.String())
+            c <- response
+            defer resp.Body.Close()
+        }(client)
+    }
+    wg.Wait()
+    close(c)
+
+    return c
+}
+
 func Distribute(clients []string, payload []byte, endpoint string, user string, pass string) <-chan model.Response {
     var wg sync.WaitGroup
     wg.Add(len(clients))
--- cbboot/salt.go
+++ cbboot/salt.go
@@ -6,6 +6,9 @@ import (
     "net/http"
     "encoding/json"
     "fmt"
+    "gopkg.in/yaml.v2"
+    "io/ioutil"
+    "os"
 )
 
 type SaltServerSetupRequest struct {
@@ -13,8 +16,41 @@ type SaltServerSetupRequest struct {
 }
 
 type SaltRunRequest struct {
-    Minions []string   `json:"minions,omitempty"`
+    Minions []SaltMinion   `json:"minions,omitempty"`
+    Server  string         `json:"server,omitempty"`
+}
+
+type SaltMinion struct {
+    Address string     `json:"address"`
+    Roles   []string   `json:"roles"`
     Server  string     `json:"server,omitempty"`
+}
+
+func (saltMinion SaltMinion) AsByteArray() []byte {
+    b, _ := json.Marshal(saltMinion)
+    return b
+}
+
+//consul:
+//    advertise_addr: 10.0.0.24
+//    recursors:
+//        - 10.0.0.2
+//        - 8.8.8.8
+//hostgroup: hostgroup_3
+//roles:
+//    - ambari_server
+//    - ambari_agent
+
+
+type ConsulGrainConfig struct {
+    AdvertiseAddr string     `json:"advertise_addr" yaml:"advertise_addr"`
+    DNSRecursors  []string   `json:"recursors" yaml:"recursors"`
+}
+
+type GrainConfig struct {
+    Consul ConsulGrainConfig       `json:"consul" yaml:"consul"`
+    //    HostGroup           string                  `json:"hostgroup yaml:"hostgroup"`
+    Roles  []string                `json:"roles" yaml:"roles"`
 }
 
 func (r SaltRunRequest) String() string {
@@ -22,18 +58,77 @@ func (r SaltRunRequest) String() string {
     return fmt.Sprintf(string(b))
 }
 
-func (salt SaltRunRequest) DistributeRun(user string, pass string) (result []model.Response) {
-    log.Printf("[distributeRun] distribute salt run command to targets: %s", salt.String())
-    for res := range Distribute(salt.Minions, nil, SaltMinionRunEP, user, pass) {
+func (saltRunRequest SaltRunRequest) distributeRun(user string, pass string) (result []model.Response) {
+    log.Printf("[distributeRun] distribute salt run command to targets: %s", saltRunRequest.String())
+    var targets []string
+    var payloads []Payload
+    for _, minion := range saltRunRequest.Minions {
+        targets = append(targets, minion.Address)
+        if minion.Server == "" {
+            minion.Server = saltRunRequest.Server
+        }
+        payloads = append(payloads, minion)
+    }
+
+    for res := range DistributePayload(targets, payloads, SaltMinionRunEP, user, pass) {
         result = append(result, res)
     }
-    result = append(result, <-Distribute([]string{salt.Server}, nil, SaltServerRunEP, user, pass))
+    result = append(result, <-Distribute([]string{saltRunRequest.Server}, nil, SaltServerRunEP, user, pass))
     return result
 }
 
 func SaltMinionRunRequestHandler(w http.ResponseWriter, req *http.Request) {
-    log.Printf("[SaltAgentRunRequestHandler] execute salt run request")
-    resp, _ := LaunchService("salt-minion")
+    log.Printf("[SaltMinionRunRequestHandler] execute salt run request")
+
+    decoder := json.NewDecoder(req.Body)
+    var saltMinion SaltMinion
+    err := decoder.Decode(&saltMinion)
+    if err != nil {
+        log.Printf("[SaltMinionRunRequestHandler] [ERROR] couldn't decode json: %s", err)
+        model.Response{Status: err.Error()}.WriteBadRequestHttp(w)
+        return
+    }
+
+    recursors := DetermineDNSRecursors([]string{})
+
+    grainConfig := GrainConfig{
+        Consul:            ConsulGrainConfig{
+            AdvertiseAddr:      saltMinion.Address,
+            DNSRecursors:       recursors,
+        },
+        Roles:             saltMinion.Roles,
+    }
+
+    grainYaml, err := yaml.Marshal(grainConfig)
+    var resp model.Response;
+    if err != nil {
+        resp = model.Response{ErrorText: err.Error(), StatusCode:http.StatusInternalServerError}
+        resp.WriteHttp(w)
+        return
+    }
+
+    err = os.MkdirAll("/etc/salt/minion.d", 0755)
+    if err != nil {
+        resp = model.Response{ErrorText: err.Error(), StatusCode:http.StatusInternalServerError}
+        resp.WriteHttp(w)
+        return
+    }
+
+    masterConf := []byte("master: " + saltMinion.Server)
+    err = ioutil.WriteFile("/etc/salt/minion.d/master.conf", masterConf, 0644)
+    if err != nil {
+        resp = model.Response{ErrorText: err.Error(), StatusCode:http.StatusInternalServerError}
+        resp.WriteHttp(w)
+        return
+    }
+
+    err = ioutil.WriteFile("/etc/salt/grains", grainYaml, 0644)
+    if err != nil {
+        resp = model.Response{ErrorText: err.Error(), StatusCode:http.StatusInternalServerError}
+        resp.WriteHttp(w)
+        return
+    }
+    resp, _ = LaunchService("salt-minion")
     resp.WriteHttp(w)
 }
 
@@ -57,8 +152,8 @@ func SaltRunDistributeRequestHandler(w http.ResponseWriter, req *http.Request) {
     log.Printf("[SaltRunDistributeRequestHandler] execute SaltRun run distribute request")
 
     decoder := json.NewDecoder(req.Body)
-    var run SaltRunRequest
-    err := decoder.Decode(&run)
+    var saltRunRequest SaltRunRequest
+    err := decoder.Decode(&saltRunRequest)
     if err != nil {
         log.Printf("[SaltRunDistributeRequestHandler] [ERROR] couldn't decode json: %s", err)
         model.Response{Status: err.Error()}.WriteBadRequestHttp(w)
@@ -66,7 +161,7 @@ func SaltRunDistributeRequestHandler(w http.ResponseWriter, req *http.Request) {
     }
 
     user, pass := GetAuthUserPass(req)
-    result := run.DistributeRun(user, pass)
+    result := saltRunRequest.distributeRun(user, pass)
     cResp := model.Responses{Responses:result}
     log.Printf("[SaltRunDistributeRequestHandler] distribute salt run command request executed: %s", cResp.String())
     json.NewEncoder(w).Encode(cResp)
