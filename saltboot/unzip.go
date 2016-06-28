efad8438c1355d916df2ac2b89ae1f919af2b2cc
mhmxs <kovacsricsi@gmail.com>
2016-06-27 17:47:28 -0700

    Unit test for unzip

--- saltboot/unzip.go
+++ saltboot/unzip.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func Unzip(src, dest string) error {
@@ -21,10 +22,20 @@ func Unzip(src, dest string) error {
 		}
 		defer rc.Close()
 
-		path := filepath.Join(dest, f.Name)
+		var path string
 		if f.FileInfo().IsDir() {
+			path = filepath.Join(dest, f.Name)
 			os.MkdirAll(path, f.Mode())
 		} else {
+			if strings.ContainsRune(f.Name, os.PathSeparator) {
+				last := strings.LastIndex(f.Name, string(os.PathSeparator))
+				dirs := f.Name[0:last]
+				name := f.Name[last+1 : len(f.Name)]
+				os.MkdirAll(filepath.Join(dest, dirs), 0744)
+				path = filepath.Join(dest, dirs, name)
+			} else {
+				path = filepath.Join(dest, f.Name)
+			}
 			f, err := os.OpenFile(
 				path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 			if err != nil {
