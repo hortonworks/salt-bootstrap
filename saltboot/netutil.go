87153bfec3e2e114a407ebf48e362e59e32a3a5b
Attila Kanto <attila.kanto@gmail.com>
2016-08-23 16:46:28 +0200

    CLOUD-57217 remove unused recursor code

--- saltboot/netutil.go
+++ saltboot/netutil.go
@@ -1,38 +1,11 @@
 package saltboot
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
-
-func DetermineDNSRecursors(fallbackDNSRecursors []string) []string {
-	var dnsRecursors []string
-	if dat, err := ioutil.ReadFile("/etc/resolv.conf"); err == nil {
-		resolvContent := string(dat)
-		log.Printf("[determineDNSRecursors] Loaded /etc/resolv.conf file: %s.", resolvContent)
-		r, _ := regexp.Compile("nameserver .*")
-		if nameserverLines := r.FindAllString(resolvContent, -1); nameserverLines != nil {
-			for _, nameserverLine := range nameserverLines {
-				log.Printf("[determineDNSRecursors] Found nameserverline: %s.", nameserverLine)
-				dnsRecursor := strings.TrimSpace(strings.Split(nameserverLine, " ")[1])
-				log.Printf("[determineDNSRecursors] Parsed DNS recursor: %s.", dnsRecursor)
-				if !strings.Contains(dnsRecursor, "127.0.0.1") {
-					dnsRecursors = append(dnsRecursors, dnsRecursor)
-				}
-			}
-		}
-	} else {
-		log.Printf("[containerhandler] Failed to load /etc/resolv.conf")
-	}
-	if fallbackDNSRecursors != nil {
-		dnsRecursors = append(dnsRecursors, fallbackDNSRecursors...)
-	}
-	return dnsRecursors
-}
 
 func DetermineBootstrapPort() int {
 
