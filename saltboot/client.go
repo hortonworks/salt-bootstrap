90fcdcc708c354dbb4ae5aac3bd2f40a15f3d25c
Tamas Bihari <tamas.bihari@sequenceiq.com>
2016-12-21 16:13:02 +0100

    use hostname and domain to update IP v6 hostname instead of hostname -f

--- saltboot/client.go
+++ saltboot/client.go
@@ -35,7 +35,7 @@ func (clients *Clients) DistributeHostnameRequest(user string, pass string) (res
 
 func ClientHostnameHandler(w http.ResponseWriter, req *http.Request) {
 	log.Printf("[ClientHostnameHandler] get FQDN")
-	fqdn, err := getHostName()
+	fqdn, err := getFQDN()
 	if err != nil {
 		log.Printf("[ClientHostnameHandler] failed to retrieve FQDN")
 		model.Response{Status: err.Error(), StatusCode: http.StatusInternalServerError}.WriteHttp(w)
