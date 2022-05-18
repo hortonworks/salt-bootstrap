b8613beddb3d764d7f868970fa495fb08eedf554
David Bajzath <bajzathd@gmail.com>
2022-05-18 11:52:34 +0200

    CB-17175 Create change password endpoint

--- saltboot/user.go
+++ saltboot/user.go
@@ -1,18 +1,25 @@
 package saltboot
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
+	"strings"
+
 	"github.com/hortonworks/salt-bootstrap/saltboot/model"
 	"github.com/kless/osutil/user/crypt/sha512_crypt"
-	"strings"
 )
 
 const (
-	SALT_USER = "saltuser"
+	SALT_USER               = "saltuser"
+	SHADOW_FILE             = "/etc/shadow"
+	SHADOW_FILE_BACKUP      = "/etc/shadow.backup"
+	SHADOW_FILE_NEW         = "/etc/shadow.new"
+	SHADOW_FILE_PERMISSIONS = 0640
 )
 
 func init() {
@@ -39,12 +46,7 @@ func CreateUser(saltMaster SaltMaster, os *Os) (resp model.Response, err error) 
 	if len(out) == 0 || err != nil {
 		log.Printf("[CreateUser] user: %s does not exsist and will be created", SALT_USER)
 
-		c := sha512_crypt.New()
-
-		// Password needs to be "salted" and must start with a magic prefix
-		salt := "$6$" + randStringRunes(20)
-
-		hash, err := c.Generate([]byte(saltMaster.Auth.Password), []byte(salt))
+		hash, err := generatePasswordHash(saltMaster.Auth.Password)
 		if err != nil {
 			return model.Response{ErrorText: err.Error(), StatusCode: http.StatusInternalServerError}, err
 		}
@@ -67,13 +69,101 @@ func CreateUser(saltMaster SaltMaster, os *Os) (resp model.Response, err error) 
 			return model.Response{ErrorText: err.Error(), StatusCode: http.StatusInternalServerError}, err
 		}
 	} else {
-		log.Printf("[CreateUser] user: %s exsist", SALT_USER)
+		log.Printf("[CreateUser] user: %s exists, setting its password", SALT_USER)
+		_, err = ChangeUserPassword(saltMaster)
+		if err != nil {
+			log.Printf("[CreateUser] ChangeUserPassword failed with error: %s", err.Error())
+		}
 	}
 
 	resp = model.Response{Status: result, StatusCode: http.StatusOK}
 	return resp, nil
 }
 
+func generatePasswordHash(password string) (string, error) {
+	// Password needs to be "salted" and must start with this prefix for SHA512 encryption
+	salt := "$6$" + randStringRunes(20)
+	return generatePasswordHashWithSalt(password, salt)
+}
+
+func generatePasswordHashWithSalt(password string, salt string) (string, error) {
+	c := sha512_crypt.New()
+	hash, err := c.Generate([]byte(password), []byte(salt))
+	if err != nil {
+		return "", err
+	}
+	return hash, nil
+}
+
 func shouldUseUserAdd(os *Os) bool {
 	return isOs(os, UBUNTU) || isOs(os, DEBIAN) || isOs(os, SUSE, SLES12)
+}
+
+func ChangeUserPassword(saltMaster SaltMaster) (resp model.Response, err error) {
+	log.Printf("[ChangeUserPassword] execute salt run request")
+
+	newPasswordHash, err := generatePasswordHash(saltMaster.Auth.Password)
+	if err != nil {
+		return errorResponse("[ChangeUserPassword] Failed to generate password hash", err)
+	}
+
+	shadowFileContents, err := readFile(SHADOW_FILE)
+	if err != nil {
+		return errorResponse("[ChangeUserPassword] Failed to read "+SHADOW_FILE, err)
+	}
+
+	shadowFileLines := strings.Split(string(shadowFileContents), "\n")
+	oldPasswordHash := ""
+	for i, shadowFileLine := range shadowFileLines {
+		if strings.HasPrefix(shadowFileLine, SALT_USER+":") {
+			oldPasswordHash = strings.Split(shadowFileLine, ":")[1]
+			shadowFileLines[i] = strings.Replace(shadowFileLine, oldPasswordHash, newPasswordHash, 1)
+			break
+		}
+	}
+
+	if oldPasswordHash == "" {
+		return errorResponse("[ChangeUserPassword] Could not find user "+SALT_USER+" in "+SHADOW_FILE, nil)
+	}
+	oldPasswordSalt := strings.Join(strings.SplitN(oldPasswordHash, "$", 2), "$")
+	newPasswordHashWithOldSalt, err := generatePasswordHashWithSalt(saltMaster.Auth.Password, oldPasswordSalt)
+	if err == nil && oldPasswordHash == newPasswordHashWithOldSalt {
+		log.Println("[ChangeUserPassword] old and new passwords are the same")
+		return model.Response{StatusCode: http.StatusOK}, nil
+	}
+
+	_, err = ExecCmd("cp", SHADOW_FILE, SHADOW_FILE_BACKUP)
+	if err != nil {
+		return errorResponse("[ChangeUserPassword] Failed to backup "+SHADOW_FILE+" to "+SHADOW_FILE_BACKUP, err)
+	}
+
+	newShadowFileContent := strings.Join(shadowFileLines, "\n")
+	err = writeFile(SHADOW_FILE_NEW, []byte(newShadowFileContent), SHADOW_FILE_PERMISSIONS)
+	if err != nil {
+		return errorResponse("[ChangeUserPassword] Failed to write new shadow file to "+SHADOW_FILE_NEW, err)
+	}
+
+	_, err = ExecCmd("mv", SHADOW_FILE_NEW, SHADOW_FILE)
+	if err != nil {
+		return errorResponse("[ChangeUserPassword] Failed to override "+SHADOW_FILE+" with "+SHADOW_FILE_NEW, err)
+	}
+
+	now := time.Now()
+	today := fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
+	_, err = ExecCmd("chage", "-d", today, SALT_USER)
+	if err != nil {
+		return errorResponse("[ChangeUserPassword] Failed to update last password change date", err)
+	}
+
+	return model.Response{StatusCode: http.StatusOK}, nil
+}
+
+func errorResponse(message string, err error) (model.Response, error) {
+	var detailedErr error
+	if err != nil {
+		detailedErr = errors.New(message + ". Error: " + err.Error())
+	} else {
+		detailedErr = errors.New(message)
+	}
+	return model.Response{ErrorText: message, StatusCode: http.StatusInternalServerError}, detailedErr
 }
