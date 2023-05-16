1a2844330ba12ecf4639969706dd0df8047c0462
David Bajzath <bajzathd@gmail.com>
2023-05-16 12:32:48 +0200

    CB-26218 Add linux arm64 build

--- release.sh
+++ release.sh
@@ -4,13 +4,11 @@ set -x
 rm -rf release
 mkdir release
 
-ARCH=$(uname -m)
-
-declare -a Platforms=("Linux" "Darwin")
-for platform in ${Platforms[@]}; do
+declare -a Platforms=("Linux_x86_64" "Darwin_x86_64" "Linux_arm64")
+for platform in "${Platforms[@]}"; do
   if [ -d "./build/$platform" ]; then
     echo "Compressing the ${platform} relevant binary ..."
-    tar -zcf "release/${BINARY}_${VERSION}_${platform}_${ARCH}.tgz" -C build/$platform $BINARY
+    tar -zcf "release/${BINARY}_${VERSION}_${platform}.tgz" -C build/$platform $BINARY
   fi
 done
 
@@ -18,7 +16,7 @@ echo "Creating release v${VERSION} from branch $BRANCH ..."
 
 output=$(gh release list | grep ${VERSION})
 if [ -z "$output" ]; then
-  gh release create "v${VERSION}" "./release/${BINARY}_${VERSION}_Linux_${ARCH}.tgz" "./release/${BINARY}_${VERSION}_Darwin_${ARCH}.tgz" -t ${VERSION} -n ""
+  gh release create "v${VERSION}" ./release/*.tgz -t ${VERSION} -n ""
 else
   echo "The cli release ${VERSION} already exists on the github."
 fi
